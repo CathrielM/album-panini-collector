321109dd32828055e956632601ad9913cc70c78b
CathrielM <vasquezsolis.goycochea@gmail.com>
2026-05-11 20:03:26 -0500

    first commit

---
+++ album-mundial/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjebWy06SY4e9RmQfs8O-QEUeHDi1SYgA",
+  authDomain: "album-mundial-5f165.firebaseapp.com",
+  projectId: "album-mundial-5f165",
+  storageBucket: "album-mundial-5f165.firebasestorage.app",
+  messagingSenderId: "543192593114",
+  appId: "1:543192593114:web:66df52760dbffc6a4bebe3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
